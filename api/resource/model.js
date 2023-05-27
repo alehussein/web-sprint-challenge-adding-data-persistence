e7ca8e1609652948b9f843526bafd50541e02ade
Alejandro Hussein <alejandrohussein1@gmail.com>
2023-05-27 12:59:04 -0500

    wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,8 @@
 // build your `Resource` model here
+function getResourceById(resource_id){
+    return Promise.resolve(`Some Resourcer whit id: ${resource_id}`)
+}
+
+module.exports = {
+    getResourceById
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,7 +1,14 @@
 // build your `/api/resources` router here
 const resourceProject = require('express').Router()
+const Resource = require('./model')
 
-
+resourceProject.get('/:resource_id', (req, res, next) =>{
+    Resource.getResourceById(req.params.resource_id)
+    .then(resourceData => {
+        res.status(200).json({resourceData})
+    })
+    .catch(next)
+})
 
 resourceProject.use((err, req, res, next) =>{ //eslint-disable-line
     res.status(500).json({
